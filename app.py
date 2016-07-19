64c2992fc3e8c1f5bc109e9f85ce54d3b02858d5
Chi Duong <chi.duong@amway.com>
2016-07-19 19:36:19 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import os
 
 @route('/')
 def index():
-	return "<h1> hello OpenShift Ninja without DB</h1>"
+	return "<h1> OpenShift Read Database</h1>"
 
 # since this is a read only talk to the replicas
 @get('/db')
